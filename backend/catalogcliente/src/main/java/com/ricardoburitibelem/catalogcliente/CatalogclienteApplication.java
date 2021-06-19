524ecb61357f68c116fad89557e1a21f7d611473
ricardobad51 <rbbelem@gmail.com>
2021-06-18 22:14:44 -0300

    first commit

---
+++ backend/catalogcliente/src/main/java/com/ricardoburitibelem/catalogcliente/CatalogclienteApplication.java
@@ -0,0 +1,13 @@
+package com.ricardoburitibelem.catalogcliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogclienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogclienteApplication.class, args);
+	}
+
+}
---
+++ backend/catalogcliente/src/test/java/com/ricardoburitibelem/catalogcliente/CatalogclienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardoburitibelem.catalogcliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogclienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
